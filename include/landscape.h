30775e852dfa0fde9de986770527ef259fbd0b26
amkev101 <123731091+amKev101@users.noreply.github.com>
2023-03-29 14:30:33 -0400

    Add files via upload

---
+++ include/landscape.h
@@ -0,0 +1,16 @@
+#ifndef LANDSCAPE_H
+#define LANDSCAPE_H
+
+
+class landscape
+{
+    public:
+        landscape();
+        virtual ~landscape();
+
+    protected:
+
+    private:
+};
+
+#endif // LANDSCAPE_H
